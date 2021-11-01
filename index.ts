cccde6b5a5f461e0f2b3f5d485807439b360c433
赵桀 <2536659921@qq.com>
2021-11-01 15:31:09 +0800

    做完了

--- index.ts
+++ index.ts
@@ -1,11 +1,28 @@
 /**
  * 请先补充完成 util.ts 的函数，再通过简单的调用来自测函数实现是否正确
- * 
+ *
  * 需要自己编写测试用例
  */
 import { Util } from "./util";
 
 // 同学们可以通过简单的调用来验证函数，如：
-console.log('Util.isNumber(11)', Util.isNumber(11));
+console.log("Util.isNumber", Util.isNumber(NaN));
 
-console.log("Util.addNumber('23', '10000000000')", Util.addNumber('23', '10000000000'));
+console.log(
+  "Util.addNumber",
+  Util.addNumber(
+    "-237856756754657576567567567567856785675",
+    "-7783642558728934758934275890352478934"
+  )
+);
+
+console.log("Util.moveZero", Util.moveZero([0, 2, 0, 14, 8, 0]));
+
+console.log("Util.isPlalindromeString", Util.isPlalindromeString("abba"));
+
+console.log("Util.formatNumber", Util.formatNumber(-134564569.3245326));
+
+console.log(
+  "Util.fillText",
+  Util.fillText("我是{{ name }}，今年{{ age }}岁", { name: "小明", age: 18 })
+);
--- util.ts
+++ util.ts
@@ -3,11 +3,11 @@ export class Util {
    * 15 分
    * 判断一个变量是否是数字
    * e.g. isNumber(4) === true, isNumber({ value: 5}) === false, isNumber('0') === false
-   * @param value 
+   * @param value
    */
   public static isNumber(value: unknown): boolean {
     // TODO 请在这里补充
-    return true;
+    return typeof value === "number";
   }
 
   /**
@@ -15,36 +15,62 @@ export class Util {
    * 千分位分隔符，用英文逗号隔开。
    * 需要考虑负数、小数
    * e.g. formatNumber(-1000000.3232) === '-1,000,000.3232'
-   * @param num 
+   * @param num
    */
   public static formatNumber(num: number): string {
     // TODO 请在这里补充
-    return '';
+    return num
+      .toString()
+      .replace(/\d+/, (str1: string): string =>
+        str1.replace(/(\d)(?=(\d{3})+$)/g, (str2: string): string => str2 + ",")
+      );
   }
 
   /**
    * 15 分
    * 模板引擎
    * {{ xxx }} 是用于替换的内容
-   * e.g. fillText('我是{{ name }}，今年{{ age }}岁', { name: '小明', gender: 18 }) === '我是小明，今年18岁'
+   * e.g. fillText('我是{{ name }}，今年{{ age }}岁', { name: '小明', age: 18 }) === '我是小明，今年18岁'
    * @param template 模板
    * @param data 填充模板的数据
    */
-  public fillText(template: string, data: object): string {
+  public static fillText(template: string, data: object): string {
     // TODO 请在这里补充
-    return '';
+    let regexp: RegExp = /\{\{\s?\w+\s?\}\}/g;
+    let result: Array<string>;
+    let ans: string = template;
+
+    while ((result = regexp.exec(template))) {
+      let key: string = result[0].match(/\w+/)[0] || "";
+      ans = ans.replace(result[0], data[key]);
+    }
+    return ans;
   }
 
-  /**
+  /**s
    * 15 分
    * 判断是否是回文字符串
    * 只考虑字母和数字字符，忽略大小写
    * e.g. isPlalindromeString('A man, a plan, a canal: Panama') === true, isPlalindromeString('abbc') === false
-   * @param str 
+   * @param str
    */
   public static isPlalindromeString(str: string): boolean {
     // TODO 请在这里补充
-    return false;
+    let newStr: string = "";
+    let len: number = str.length;
+    str = str.toLowerCase();
+    for (let i: number = 0; i < len; i++) {
+      if ((str[i] >= "a" && str[i] <= "z") || (str[i] >= "0" && str[i] <= "9"))
+        newStr += str[i];
+    }
+    len = newStr.length;
+    let l: number = 0,
+      r: number = len - 1;
+    while (l < r) {
+      if (newStr[l] !== newStr[r]) return false;
+      l++, r--;
+    }
+    return true;
   }
 
   /**
@@ -52,12 +78,22 @@ export class Util {
    * 向后移动零
    * 给定一个数组 numbers，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
    * e.g. moveZero([0, 2, 0, 14, 8, 0]) === [2, 14, 8, 0, 0, 0]
-   * @param numbers 
-   * @returns 
+   * @param numbers
+   * @returns
    */
-  public moveZero(numbers: number[]): number[] {
+  public static moveZero(numbers: number[]): number[] {
     // TODO 请在这里补充
-    return [];
+    const len: number = numbers.length;
+    let l: number = 0,
+      r: number = 0;
+    while (r < len) {
+      if (numbers[r] !== 0) {
+        [numbers[l], numbers[r]] = [numbers[r], numbers[l]];
+        l++;
+      }
+      r++;
+    }
+    return numbers;
   }
 
   /**
@@ -65,11 +101,11 @@ export class Util {
    * 大数相加
    * 需要考虑负数
    * e.g. addNumber('1000000000000000', '-1000000000000000') === '0
-   * @param numstr1 
-   * @param numstr2 
+   * @param numstr1
+   * @param numstr2
    */
   public static addNumber(numstr1: string, numstr2: string): string {
     // TODO 请在这里补充
-    return '0';
+    return (BigInt(numstr1) + BigInt(numstr2)).toString();
   }
 }
